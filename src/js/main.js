7ec27a5dfe76ecbf0b0f019b685ff71d3b4fcb3d
Korotkyy <118772636+Korotkyy@users.noreply.github.com>
2024-03-18 05:18:57 +0100

    'update'

---
+++ src/js/main.js
@@ -0,0 +1,1 @@
+import './1-timer.html';
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,30 @@
 import { defineConfig } from 'vite';
+import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig({
-  base: '/goit-js-hw-10/',
-  root: './src',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        'index': '/1-timer.html', // Указываем точный путь к вашему HTML-файлу
-      },
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
     },
-  },
-  plugins: [
-    injectHTML({
-      injectData: {
-        title: 'Your App Title',
+    root: 'src',
+    build: {
+      sourcemap: true,
+
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: 'commonHelpers.js',
+        },
       },
-    }),
-  ],
+      outDir: '../dist',
+    },
+    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  };
 });
